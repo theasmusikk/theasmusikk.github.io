a63c099b1d28503eafa45f3273c55766014004d7
theasmusikk <131396059+theasmusikk@users.noreply.github.com>
2024-03-16 22:53:18 +0100

    Update kon.js

--- kon.js
+++ kon.js
@@ -6,48 +6,56 @@ var urls = {
       'url': 'https://open.spotify.com/track/0CJDroZzR6PNalEaAsJJm0?si=4f2ab6208ac74103', //'https://spoti.fi/3ZdtBHQ?r=lp'
       'gad_id': 'AW-11140570940/qrN6CKDV65kYELy-nsAp',
       'gad_val': 0.01,
+      'm_id'= ['1'],
       'm_content_type': 'streaming'
     },
     'youtube': {
       'url': 'https://youtu.be/LM23SRzMcMw?si=1tJKzy97fFIR0uOo', //'https://www.youtube.com/@theasmusikk',
       'gad_id': 'AW-11140570940/rnw4CKPV65kYELy-nsAp',
       'gad_val': 0.001,
+      'm_id'= ['2'],
       'm_content_type': 'some'
     },
     'applemusic': {
       'url': 'https://music.apple.com/no/album/hjulene-p%C3%A5-bussen/1676160459?i=1676160460', //'https://apple.co/3K1bXS1?r=lp',
       'gad_id': 'AW-11140570940/UB6LCKbV65kYELy-nsAp',
       'gad_val': 0.01,
+      'm_id'= ['3'],
       'm_content_type': 'streaming'
     },
     'tidal': {
       'url': 'https://tidal.com/browse/track/280593811', 
       'gad_id': 'AW-11140570940/UB6LCKbV65kYELy-nsAp',
       'gad_val': 0.01,
+        'm_id'= ['4'],
       'm_content_type': 'streaming'
     },
     'youtubemusic': {
       'url': 'https://music.youtube.com/watch?v=AflZLAKpTbw&si=CCE5EUGJCx_s_TFR', //'https://music.youtube.com/channel/UCF7ZKFhkEaEkeow-Tmd9frA?feature=share',
       'gad_id': 'AW-11140570940/juhECKnV65kYELy-nsAp',
       'gad_val': 0.005,
+      'm_id'= ['5'],
       'm_content_type': 'streaming'
     },
     'andre': {
       'url': 'https://bit.ly/blinke-blinke-stjernelill?r=lp',
       'gad_id': 'AW-11140570940/wKh5CMar-5kYELy-nsAp',
       'gad_val': 0.0001,
+      'm_id'= ['6'],
       'm_content_type': 'some'
     },
     'instagram': {
       'url': 'https://www.instagram.com/theasmusikk/',
       'gad_id': 'AW-11140570940/vE_aCKzV65kYELy-nsAp',
       'gad_val': 0.0001,
+      'm_id'= ['7'],
       'm_content_type': 'some'
     },
     'facebook': {
       'url': 'https://www.facebook.com/theasmusikk',
       'gad_id': 'AW-11140570940/uEosCK_V65kYELy-nsAp',
       'gad_val': 0.0001,
+      'm_id'= ['8'],
       'm_content_type': 'some'  
     }
   };
